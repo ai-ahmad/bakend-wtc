ef91a9e120117b5dd355a8ca10c4a9bb0331c397
ai-ahmad <ahmedovalmat8@gmail.com>
2024-10-22 14:15:11 +0500

    tes

---
+++ src/router/AuthRouter.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bcrypt = require('bcrypt'); 
+const User = require('../models/UserModels'); 
+
+const router = express.Router();
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({ username });
+        if (!user) return res.status(404).json({ message: 'User not found' });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(401).json({ message: 'Invalid password' });
+        res.json({ user });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.post('/create-partnyor', async (req, res) => {
+    const { username, password, role } = req.body;
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ username, password: hashedPassword, role }); 
+        await newUser.save(); 
+        res.status(201).json({ message: 'User created successfully' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+module.exports = router;
