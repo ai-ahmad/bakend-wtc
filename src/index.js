0f015e5c7a30289918cd73c4c7b59c3f4ebc5eeb
ai-ahmad <ahmedovalmat8@gmail.com>
2024-10-22 14:19:22 +0500

    test

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const connectDB = require('./config/db');
 const  ProductRouter = require('./router/ProductRouter')
 const BannerRouter = require('./router/BannerRouter')
+const AuthRouter = require('./router/AuthRouter')
 connectDB()
 
 const app = express();
 app.use('/api/v1/products', ProductRouter)
-// app.use('/api/v1/banners', BannerRouter)
+app.use('/api/v1/banners', BannerRouter)
+app.use('/api/v1/auth', AuthRouter)
 
 
 const PORT = 9000;
