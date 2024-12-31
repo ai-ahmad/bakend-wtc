e5a4cf2c80d58e0bf49d6bbf91e0637d7a80a2dd
ai-ahmad <ahmedovalmat8@gmail.com>
2024-12-31 07:42:54 +0500

    all codes

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const uri = process.env.URI
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(uri || "mongodb+srv://Abdulloh:634571@cluster0.76u0c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => console.log("Connected to MongoDB using Mongoose!")).catch(err => console.log("Error connecting"))
+    await mongoose.connect(uri || "mongodb+srv://admin:admin@cluster0.z8xfi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => console.log("Connected to MongoDB using Mongoose!")).catch(err => console.log("Error connecting"))
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1);
