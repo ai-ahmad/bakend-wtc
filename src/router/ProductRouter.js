4854033c1544f908d2e51fc825f2c449e12f0e31
ai-ahmad <ahmedovalmat8@gmail.com>
2025-01-01 13:37:52 +0500

    test

--- src/router/ProductRouter.js
+++ src/router/ProductRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const Product = require('../models/ProductModel'); // Ensure the correct path
+const Product = require('../models/ProdutModel'); // Ensure the correct path
 
 const router = express.Router();
 
